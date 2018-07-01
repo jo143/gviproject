7a9da15943db93b01416764676f1969cf034bf9c
satheesh kannan <smarteye9626@gmail.com>
2018-07-01 21:27:38 +0530

    Create guvi50.java
    at guvi repositry

---
+++ guvi50.java
@@ -0,0 +1,18 @@
+package csvmain;
+import java.io.*;
+import java.util.*;
+public class guvi50 {
+
+	public static void main(String[] args) {
+	
+		Scanner ob=new Scanner(System.in);
+		String name=ob.nextLine();
+		int n=ob.nextInt();
+		for(int i=0;i<=n;i++)
+		{
+			System.out.println(name);
+		}
+
+	}
+
+}
